49d04c2296f8a9d6410c3ffab820682ebe369d41
maxwellgrossman <maxperryg@gmail.com>
2019-04-08 16:05:37 -0400

    Sussessfully showing grocery list names, each with checkboxes in front of them

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GListViewAdapter.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GListViewAdapter.java
@@ -1,29 +1,44 @@
 package edu.qc.seclass.glm;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class GListViewAdapter extends ArrayAdapter<String> {
+public class GListViewAdapter extends ArrayAdapter<GroceryList> {
 
-    private final Activity context;
-    private final ArrayList<GroceryList> mainList;
+    ArrayList<GroceryList> mainList = new ArrayList<>();
 
-    public GListViewAdapter(Activity context, String name, ArrayList<GroceryList> mainList){
-        super(context, android.R.layout.simple_list_item_1;
-        this.context = context;
+    public GListViewAdapter(Context context, int textViewResourceId, ArrayList<GroceryList> mainList) {
+        super(context, textViewResourceId, mainList);
         this.mainList = mainList;
     }
 
-    public View getView(View view, ViewGroup parent){
-        LayoutInflater inflater = context.getLayoutInflater();
-        View rowView = inflater.inflate(android.R.layout.simple_list_item_1, null, true);
-
-        TextView title = (TextView) rowView.findViewById(R.id.)
+    @Override
+    public int getCount() {
+        return super.getCount();
     }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        v = inflater.inflate(R.layout.g_list_view_items, null);
+        TextView textView = (TextView) v.findViewById(R.id.textView);
+        CheckBox imageView = (CheckBox) v.findViewById(R.id.chk_box);
+        textView.setText(mainList.get(position).getName());
+        return v;
+
+    }
+
 }
+
--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
@@ -21,24 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listOfLists);
 
-        final ArrayList<String> addGroceryList = new ArrayList<>();
+        ListOfLists mainList = new ListOfLists();
+        mainList.add(new GroceryList("Sample Grocery List 1"));
+        mainList.add(new GroceryList("Sample Grocery List 2"));
 
-        addGroceryList.add("Brachs Grocery List");
-        addGroceryList.add("Seasons Grocery List");
-        addGroceryList.add("Gourmet Glatt Grocery List");
-        addGroceryList.add("Trader Joes Grocery List");
-        addGroceryList.add("Supersol Grocery List");
+        GListViewAdapter myAdapter = new GListViewAdapter(this,R.layout.g_list_view_items, mainList);
+        listView.setAdapter(myAdapter);
 
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, addGroceryList);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Selected List: " + addGroceryList.get(position).toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
+//        final ArrayList<String> addGroceryList = new ArrayList<>();
+//
+//        addGroceryList.add("Brachs Grocery List");
+//        addGroceryList.add("Seasons Grocery List");
+//        addGroceryList.add("Gourmet Glatt Grocery List");
+//        addGroceryList.add("Trader Joes Grocery List");
+//        addGroceryList.add("Supersol Grocery List");
+//
+//
+//        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, addGroceryList);
+//        listView.setAdapter(arrayAdapter);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this, "Selected List: " + addGroceryList.get(position).toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 //        FloatingActionButton floatingActionButton= findViewById(R.id.addItemButton);
 //        floatingActionButton.setOnClickListener(new View.OnClickListener() {
