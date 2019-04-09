8e886609d09d82613e35716af9f8b81c37404049
maxwellgrossman <maxperryg@gmail.com>
2019-04-08 20:59:00 -0400

    open grocery list page, need ot figure out how to pass the grocery list

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
@@ -1,13 +1,18 @@
 package edu.qc.seclass.glm;
 
 //import android.support.design.widget.FloatingActionButton;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -17,6 +22,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     GridView listView;
+    ListOfLists mainList = new ListOfLists();
+    final Context context = this;
+    private EditText result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.listOfLists);
 
-        ListOfLists mainList = new ListOfLists();
-        mainList.add(new GroceryList("Sample Grocery List 1selakhfjrhgjdfaf;as;hfdkahljfz;gbjzl"));
-        mainList.add(new GroceryList("Sample Grocery List 2"));
-        mainList.add(new GroceryList("Sample Grocery List 3"));
-        mainList.add(new GroceryList("Sample Grocery List 4"));
-        mainList.add(new GroceryList("Sample Grocery List 5"));
-        mainList.add(new GroceryList("Sample Grocery List 6"));
-        mainList.add(new GroceryList("Sample Grocery List 7"));
-        mainList.add(new GroceryList("Sample Grocery List 8"));
 
-        GListViewAdapter myAdapter = new GListViewAdapter(this,R.layout.g_list_view_items, mainList);
+        final GListViewAdapter myAdapter = new GListViewAdapter(this,R.layout.g_list_view_items, mainList);
         listView.setAdapter(myAdapter);
         listView.setClickable(true);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.print("psarent" + parent+ "view"+ view + "p" + position + "id"+id);
                 openGList();
             }
         });
@@ -49,14 +50,51 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-              //     openNameList();
-                    Toast.makeText(MainActivity.this, "Add Grocery List!", Toast.LENGTH_SHORT).show();
+                    addList(myAdapter);
+                    myAdapter.notifyDataSetChanged();
                 }
         });
     }
     public void openGList(){
         Intent intent =  new Intent(this, gListActivity.class);
         startActivity(intent);
+    }
+
+    public void addList(final GListViewAdapter myAdapter){
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.g_list_name_prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.nameInput);
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                myAdapter.add(new GroceryList(userInput.getText().toString()));
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+
     }
 
 
--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/gListActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/gListActivity.java
@@ -1,13 +1,29 @@
 package edu.qc.seclass.glm;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class gListActivity extends AppCompatActivity {
+
+    ListView groceryListView;
+    GroceryList gList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_g_list);
+
+        FloatingActionButton floatingActionButton= findViewById(R.id.addItemButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                addList(myAdapter);
+//                myAdapter.notifyDataSetChanged();
+            }
+        });
+   // }
     }
 }
