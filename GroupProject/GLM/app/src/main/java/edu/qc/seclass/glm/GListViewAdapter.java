bcbdad1b6f686d488f1845adea26fa0e86751ece
maxwellgrossman <maxperryg@gmail.com>
2019-04-08 20:06:44 -0400

    created a grid layout for the list of lists, need to figure out how to place the add button in front of the list items

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GListViewAdapter.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/GListViewAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,8 +35,10 @@ public class GListViewAdapter extends ArrayAdapter<GroceryList> {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         v = inflater.inflate(R.layout.g_list_view_items, null);
         TextView textView = (TextView) v.findViewById(R.id.textView);
-        CheckBox imageView = (CheckBox) v.findViewById(R.id.chk_box);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+//        CheckBox imageView = (CheckBox) v.findViewById(R.id.chk_box);
         textView.setText(mainList.get(position).getName());
+        imageView.setImageResource(R.mipmap.gridimage);
         return v;
 
     }
--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
@@ -1,18 +1,22 @@
 package edu.qc.seclass.glm;
 
-import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.FloatingActionButton;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+    GridView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +25,19 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listOfLists);
 
         ListOfLists mainList = new ListOfLists();
-        mainList.add(new GroceryList("Sample Grocery List 1"));
+        mainList.add(new GroceryList("Sample Grocery List 1selakhfjrhgjdfaf;as;hfdkahljfz;gbjzl"));
         mainList.add(new GroceryList("Sample Grocery List 2"));
+        mainList.add(new GroceryList("Sample Grocery List 3"));
+        mainList.add(new GroceryList("Sample Grocery List 4"));
+        mainList.add(new GroceryList("Sample Grocery List 5"));
+        mainList.add(new GroceryList("Sample Grocery List 6"));
+        mainList.add(new GroceryList("Sample Grocery List 7"));
+        mainList.add(new GroceryList("Sample Grocery List 8"));
 
         GListViewAdapter myAdapter = new GListViewAdapter(this,R.layout.g_list_view_items, mainList);
         listView.setAdapter(myAdapter);
         listView.setClickable(true);
-        listView.setOnItemClickListener(new OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 openGList();
@@ -56,4 +66,11 @@ public class MainActivity extends AppCompatActivity {
 //    }
 //
 
-}
+//        FloatingActionButton floatingActionButton= findViewById(R.id.addItemButton);
+//        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Toast.makeText(MainActivity.this, "Add Item!", Toast.LENGTH_SHORT).show();
+//                }
+//        });
+    }
