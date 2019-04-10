7f05bc4e599d2c70b5c83fa785f043da6cc4b245
maxwellgrossman <maxperryg@gmail.com>
2019-04-10 12:17:46 -0400

    Added a prompt inside the grocery list screen to add items. Now it only takes the name of an Item - needs to show a list of item types, and then Item names. Also, It does not save the state of the grocery list after leaving the screen for that grocery list

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/gListActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/gListActivity.java
@@ -1,19 +1,25 @@
 package edu.qc.seclass.glm;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class gListActivity extends AppCompatActivity {
 
     ListView groceryListView;
     GroceryList gList;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +35,52 @@ public class gListActivity extends AppCompatActivity {
         groceryListView.setAdapter(myAdapter);
         groceryListView.setClickable(true);
 
-        myAdapter.add(new Item("Cereal", "Cherios"));
-        myAdapter.add(new Item("Cereal", "some other cereal"));
-
         myAdapter.notifyDataSetChanged();
 
         FloatingActionButton floatingActionButton= findViewById(R.id.addItemButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                addList(myAdapter);
-//                myAdapter.notifyDataSetChanged();
+                addList(myAdapter);
+                myAdapter.notifyDataSetChanged();
             }
         });
-   // }
+    }
+
+    public void addList(final GroceryListArrayAdapter myAdapter){
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.add_item_prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.nameInput);
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                myAdapter.add(new Item("Sample Type", userInput.getText().toString()));
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+
     }
 }
